85fc49227140f4815be4cbc8b9865f7ce7620a57
Zamor Marc Philippe <marc_philippe.zamor@student.ueh.edu.ht>
2026-01-05 14:39:08 -0500

    Reset complet du repository – base propre C#

---
+++ GestionReservations/Domaine/StatutRessource.cs
@@ -0,0 +1,8 @@
+public enum StatutRessource
+{
+    EnReparation, 
+    Reservee,
+    Occupee,
+    Disponible
+}
+
---
+++ GestionReservations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestionReservations.Domaine;
+
+namespace GestionReservations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue au système de gestion de réservations de la CENTRALEO Services!");
+        }
+    }
+}
